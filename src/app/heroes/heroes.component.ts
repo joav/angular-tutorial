001f30e133b5d21bf504342faa7269963fa12685
joav <jim.acero.valcarcel@gmail.com>
2019-02-04 00:09:49 -0500

    Primeras dos parts del tutorial inicial terminado: - *ngFor - *ngIf - class.selected - ngModel - FormModule

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() { }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+  ngOnInit() {
+  }
+
+}
