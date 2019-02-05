3ff9ecd16bdb5f333765257db4925f1ed0a9657a
joav <jim.acero.valcarcel@gmail.com>
2019-02-04 22:29:04 -0500

    Angular tutorial: - Separamos los detalles de un heroe en un componente - Creamos el servicio para pedir heroes - Incluimos el nuevo componente para renderizar mensajes - Incluimos un nuevo servicio para crear mensajes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../Hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,16 +15,22 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
